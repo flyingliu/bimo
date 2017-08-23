a44599e4dace6e14d257b7292bddae4fcfe7271f
flyngliu <fly0733@gmail.com>
2017-08-23 18:14:48 +0800

    wre

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,14 +1,35 @@
 <template>
-    <div class="main index">
-        <h1>this in index</h1>
+    <div class="main index" id="index" :title="msg">
+        <div class="scroll">
+            <img class="img" :src="img"/>
+        </div>
     </div>
 </template>
 <script>
+var myScroll;
 export default {
     data() {
         return {
-            msg: "this flying"
+            msg: "this flying",
+            img:''
         }
+    },
+
+    mounted() {
+        this.img = "http://ww1.sinaimg.cn/large/006poVAQgy1fitscuifcjj318g0m8gpl.jpg";
+        var imgData = new Image(this.img);
+        imgData.src = this.img
+        imgData.onload = (abc) => {
+            console.log(abc);
+        }
+        console.log(this);
+        myScroll = new IScroll('#index', {
+            zoom: true,
+            scrollX: true,
+            scrollY: true,
+            mouseWheel: true,
+            wheelAction: 'zoom'
+        });
     }
 }
 </script>
