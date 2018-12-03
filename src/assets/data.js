3b9e9a1873617112ed720127a4b01e9c132a0775
flying <fly0733@gmail.com>
2018-12-03 16:32:35 +0800

    12

--- src/assets/data.js
+++ src/assets/data.js
@@ -4,107 +4,107 @@ export default {
     {
       type: '楷书',
       time: '2017.08.13',
-      img: 'http://ov61aed6n.bkt.clouddn.com/17-8-24/18610900.jpg',
+      img: 'http://photo.guolaijie.com/17-8-24/18610900.jpg',
       text: '帝被弑一恸而绝梁武深恨之事见梁周北齐书生梁镇西记室参军讳',
-      thumb:"http://ov61aed6n.bkt.clouddn.com/17-8-24/20733974.jpg"
+      thumb:"http://photo.guolaijie.com/17-8-24/20733974.jpg"
     }, {
       type: '草书',
       time: '2017.07.13',
-      img: 'http://ov61aed6n.bkt.clouddn.com/17-8-24/48075678.jpg',
+      img: 'http://photo.guolaijie.com/17-8-24/48075678.jpg',
       text: '裴将军！大君制六合，猛将清九垓。战马若龙虎，腾陵何壮哉。将军临北荒，烜赫耀英材。剑舞跃游电，随风萦且回。登高望天山，白云正崔嵬。入阵破骄虏，威声雄震雷。一射百马倒，再射万夫开。匈奴不敢敌，相呼归去来。功成报天子，可以画麟台。',
-      thumb: "http://ov61aed6n.bkt.clouddn.com/17-8-24/72441935.jpg"
+      thumb: "http://photo.guolaijie.com/17-8-24/72441935.jpg"
     }, {
       type: '楷书',
       time: '2017.07.13',
-      img: 'http://ov61aed6n.bkt.clouddn.com/17-8-24/6517187.jpg',
+      img: 'http://photo.guolaijie.com/17-8-24/6517187.jpg',
       text: '颜真卿：晋侍中',
-      thumb: "http://ov61aed6n.bkt.clouddn.com/17-8-24/48447950.jpg"
+      thumb: "http://photo.guolaijie.com/17-8-24/48447950.jpg"
     }, {
       type: '行书',
       time: '2017.07.13',
-      img: 'http://ov61aed6n.bkt.clouddn.com/17-8-24/66484611.jpg',
+      img: 'http://photo.guolaijie.com/17-8-24/66484611.jpg',
       text: '一止为正',
-      thumb: "http://ov61aed6n.bkt.clouddn.com/17-8-24/47133669.jpg"
+      thumb: "http://photo.guolaijie.com/17-8-24/47133669.jpg"
     }, {
       type: '行书',
       time: '2017.07.3',
-      img: 'http://ov61aed6n.bkt.clouddn.com/17-8-24/54846249.jpg',
+      img: 'http://photo.guolaijie.com/17-8-24/54846249.jpg',
       text: '色即是空',
-      thumb: "http://ov61aed6n.bkt.clouddn.com/17-8-24/79235630.jpg"
+      thumb: "http://photo.guolaijie.com/17-8-24/79235630.jpg"
     }, {
       type: '楷书',
       time: '2017.07.3',
-      img: 'http://ov61aed6n.bkt.clouddn.com/17-8-24/90008547.jpg',
+      img: 'http://photo.guolaijie.com/17-8-24/90008547.jpg',
       text: '颜真卿：文赋',
-      thumb: "http://ov61aed6n.bkt.clouddn.com/17-8-24/56202044.jpg"
+      thumb: "http://photo.guolaijie.com/17-8-24/56202044.jpg"
     }, {
       type: '楷书',
       time: '2017.07.3',
-      img: 'http://ov61aed6n.bkt.clouddn.com/17-8-24/21958626.jpg',
+      img: 'http://photo.guolaijie.com/17-8-24/21958626.jpg',
       text: '颜真卿：鲁公',
-      thumb: "http://ov61aed6n.bkt.clouddn.com/17-8-24/43687870.jpg"
+      thumb: "http://photo.guolaijie.com/17-8-24/43687870.jpg"
     }, {
       type: '楷书',
       time: '2017.07.3',
-      img: 'http://ov61aed6n.bkt.clouddn.com/17-8-24/89378471.jpg',
+      img: 'http://photo.guolaijie.com/17-8-24/89378471.jpg',
       text: '颜真卿：自教',
-      thumb: "http://ov61aed6n.bkt.clouddn.com/17-8-24/27447060.jpg"
+      thumb: "http://photo.guolaijie.com/17-8-24/27447060.jpg"
     }, {
       type: '楷书',
       time: '2017.07.3',
-      img: 'http://ov61aed6n.bkt.clouddn.com/17-8-24/49113851.jpg',
+      img: 'http://photo.guolaijie.com/17-8-24/49113851.jpg',
       text: '书山有路勤为径，学海无涯苦作舟',
-      thumb: "http://ov61aed6n.bkt.clouddn.com/17-8-24/33704287.jpg"
+      thumb: "http://photo.guolaijie.com/17-8-24/33704287.jpg"
     }, {
       type: '草书',
       time: '2017.07.3',
-      img: 'http://ov61aed6n.bkt.clouddn.com/17-8-27/12240440.jpg',
+      img: 'http://photo.guolaijie.com/17-8-27/12240440.jpg',
       text: '松下问童子，言师采药去，只在此山中，云深不知处。',
-      thumb: "http://ov61aed6n.bkt.clouddn.com/17-8-27/17917049.jpg"
+      thumb: "http://photo.guolaijie.com/17-8-27/17917049.jpg"
     }, {
       type: '草书',
       time: '2017.08.21',
-      img: 'http://ov61aed6n.bkt.clouddn.com/17-8-27/12240440.jpg',
+      img: 'http://photo.guolaijie.com/17-8-27/12240440.jpg',
       text: '白日依山尽，黄河入海流，欲穷千里目，更上一层楼',
-      thumb: "http://ov61aed6n.bkt.clouddn.com/17-8-27/47013203.jpg"
+      thumb: "http://photo.guolaijie.com/17-8-27/47013203.jpg"
     }, {
       type: '草书',
       time: '2017.08.21',
-      img: 'http://ov61aed6n.bkt.clouddn.com/17-8-27/44515734.jpg',
+      img: 'http://photo.guolaijie.com/17-8-27/44515734.jpg',
       text: '锄禾日当午，汗滴禾下土，谁知盘中餐，粒粒皆辛苦',
-      thumb: "http://ov61aed6n.bkt.clouddn.com/17-8-27/71975634.jpg"
+      thumb: "http://photo.guolaijie.com/17-8-27/71975634.jpg"
     }, 
     
     {
       type: '行书',
       time: '2017.09.13',
-      img: 'http://ov61aed6n.bkt.clouddn.com/17-9-13/47551893.jpg',
+      img: 'http://photo.guolaijie.com/17-9-13/47551893.jpg',
       text: '人间至味是清欢',
-      thumb: "http://ov61aed6n.bkt.clouddn.com/17-9-13/94732382.jpg"
+      thumb: "http://photo.guolaijie.com/17-9-13/94732382.jpg"
     }, {
       type: '行书',
       time: '2017.09.13',
-      img: 'http://ov61aed6n.bkt.clouddn.com/17-9-13/5263265.jpg',
+      img: 'http://photo.guolaijie.com/17-9-13/5263265.jpg',
       text: '人间至味是清欢',
-      thumb: "http://ov61aed6n.bkt.clouddn.com/17-9-13/85016984.jpg"
+      thumb: "http://photo.guolaijie.com/17-9-13/85016984.jpg"
     }, {
       type: '行书',
       time: '2017.09.13',
-      img: 'http://ov61aed6n.bkt.clouddn.com/17-9-13/68610049.jpg',
+      img: 'http://photo.guolaijie.com/17-9-13/68610049.jpg',
       text: '花好月圆',
-      thumb: "http://ov61aed6n.bkt.clouddn.com/17-9-13/14216953.jpg"
+      thumb: "http://photo.guolaijie.com/17-9-13/14216953.jpg"
     }, {
       type: '行书',
       time: '2017.09.13',
-      img: 'http://ov61aed6n.bkt.clouddn.com/17-9-13/70734569.jpg',
+      img: 'http://photo.guolaijie.com/17-9-13/70734569.jpg',
       text: '天作之合',
-      thumb: "http://ov61aed6n.bkt.clouddn.com/17-9-13/76874427.jpg"
+      thumb: "http://photo.guolaijie.com/17-9-13/76874427.jpg"
     }, {
       type: '行书',
       time: '2017.09.13',
-      img: 'http://ov61aed6n.bkt.clouddn.com/17-9-13/89642836.jpg',
+      img: 'http://photo.guolaijie.com/17-9-13/89642836.jpg',
       text: '人生得意须尽欢',
-      thumb: "http://ov61aed6n.bkt.clouddn.com/17-9-13/67535135.jpg"
+      thumb: "http://photo.guolaijie.com/17-9-13/67535135.jpg"
     }
 
   ]
